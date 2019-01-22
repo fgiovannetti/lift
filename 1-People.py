32da2890f2d01cb1feb36800d1c51a435dc524cf
Francesca Giovannetti <francesca.giovannetti91@gmail.com>
2019-01-22 11:31:27 +0000

    1-People extract created

--- 1-People.py
+++ 1-People.py
@@ -33,7 +33,7 @@ def sameas(person):
     sameAs = person.get('sameAs').split()
     i = 0
     while i < len(sameAs):
-        o.write('  <owl:sameAs rdf:resource="' + sameAs[i] + '"/>')
+        o.write('<owl:sameAs rdf:resource="' + sameAs[i] + '"/>')
         i += 1
 
 
@@ -42,9 +42,9 @@ def persname(person):
     label = persName.text
     label_lang = persName.get('{http://www.w3.org/XML/1998/namespace}lang')
     if label_lang is not None:
-        o.write('  <rdfs:label ' + 'xml:lang="' + label_lang + '">' + label + '</rdfs:label>')
+        o.write('<rdfs:label ' + 'xml:lang="' + label_lang + '">' + label + '</rdfs:label>')
     else:
-        o.write('  <rdfs:label>' + label + '</rdfs:label>')
+        o.write('<rdfs:label>' + label + '</rdfs:label>')
 
 
 def referenced_person(person_id):
@@ -52,7 +52,7 @@ def referenced_person(person_id):
     for referenced_person in root.findall(ref, ns):
         parent = referenced_person.getparent()
         parent_id = parent.get('{http://www.w3.org/XML/1998/namespace}id')
-        o.write('  <dcterms:isReferencedBy rdf:resource="' + base_uri + '/text/' + parent_id + '"/>')
+        o.write('<dcterms:isReferencedBy rdf:resource="' + base_uri + '/text/' + parent_id + '"/>')
 
 
 def referencing_fragment(person_id):
@@ -61,8 +61,8 @@ def referencing_fragment(person_id):
         parent = referencing_fragment.getparent()
         parent_id = parent.get('{http://www.w3.org/XML/1998/namespace}id')
         o.write('<rdf:Description rdf:about="' + base_uri + '/text/' + parent_id + '">')
-        o.write('  <rdf:type rdf:resource="http://iflastandards.info/ns/fr/frbr/frbroo/F23_Expression_Fragment"/>')
-        o.write('  <frbroo:R15i_is_fragment_of rdf:resource="' + base_uri + '"/>')
+        o.write('<rdf:type rdf:resource="http://iflastandards.info/ns/fr/frbr/frbroo/F23_Expression_Fragment"/>')
+        o.write('<frbroo:R15i_is_fragment_of rdf:resource="' + base_uri + '"/>')
         o.write('</rdf:Description>')
 
 
@@ -71,16 +71,16 @@ def perstype(person):
     perstype = listPerson.get('type')
     perscorr = listPerson.get('corresp')
     if perstype is not None:
-        o.write('  <dcterms:description>' + perstype + '</dcterms:description>')
+        o.write('<dcterms:description>' + perstype + '</dcterms:description>')
     if perscorr is not None and perscorr.startswith('http'):
-        o.write('  <dcterms:subject rdf:resource="' + perscorr + '"/>')
+        o.write('<dcterms:subject rdf:resource="' + perscorr + '"/>')
 
 
 for person in root.findall('.//tei:person', ns):
     person_id = person.get('{http://www.w3.org/XML/1998/namespace}id')
     person_ref = '#' + person_id
     subject(person)
-    o.write('  <rdf:type rdf:resource="https://schema.org/Person"/>')
+    o.write('<rdf:type rdf:resource="https://schema.org/Person"/>')
     sameas(person)
     persname(person)
     referenced_person(person_id)
