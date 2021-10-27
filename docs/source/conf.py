97bd64a576bc822f3fc060a977ca1ee937df49e7
Francesca Giovannetti <francesca.giovannetti91@gmail.com>
2021-10-27 10:39:41 +0200

    Updating docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LIFT'
+copyright = '2021, Francesca Giovannetti'
+author = 'Francesca Giovannetti'
 
 release = '0.1'
 version = '0.1.0'
